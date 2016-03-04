746c0d6486c8ed44526cee8c48e06098e2b2ec40
lulin <lulin@beisen.com>
2016-03-05 00:15:01 +0800

    add jsp page and implement not annotation handler

--- springmvcfirst/src/cn/richard/ssm/controller/ItemsController1.java
+++ springmvcfirst/src/cn/richard/ssm/controller/ItemsController1.java
@@ -1,17 +1,42 @@
 package cn.richard.ssm.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import cn.richard.ssm.pojo.Items;
+
 public class ItemsController1 implements Controller{
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		List<Items> itemList=new ArrayList();
+		
+		Items item1=new Items();
+		item1.setId(100);
+		item1.setName("Lenovo notebook");
+		item1.setPrice(6899f);
+		item1.setDetail("Thinkpad E450联想笔记本电脑");
+		itemList.add(item1);
+		
+		Items item2=new Items();
+		item2.setId(101);
+		item2.setName("Apple mobile");
+		item2.setPrice(5808f);
+		item2.setDetail("Apple mobile 6s Plus");
+		itemList.add(item2);
+		
+		ModelAndView view=new ModelAndView();
+		view.addObject("itemList", itemList);
+		view.setViewName("/WEB-INF/jsp/items/itemsList.jsp");
+		
+		return view;
 	}
 
 }
--- springmvcfirst/src/cn/richard/ssm/pojo/Items.java
+++ springmvcfirst/src/cn/richard/ssm/pojo/Items.java
@@ -1,5 +1,55 @@
 package cn.richard.ssm.pojo;
 
-public class Items {
+import java.util.Date;
 
+public class Items {
+	public int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String name;
+	
+	public float price;
+	
+	public String Detail;
+	
+	public Date createtime;
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDetail() {
+		return Detail;
+	}
+
+	public void setDetail(String detail) {
+		Detail = detail;
+	}
 }
